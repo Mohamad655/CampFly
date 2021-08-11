5317bae6e0e5e58f8dde2dcc33a443fc881089f1
Mohammad 1 <md.haq7892@gmail.com>
2021-08-11 14:14:27 +0530

    adding cluster map

--- models/campground.js
+++ models/campground.js
@@ -3,6 +3,10 @@ const Review = require('./review');
 const User = require('./user');
 const Schema = mongoose.Schema;
 
+
+const opts = { toJSON: { virtuals: true } };
+
+
 const ImageSchema = new Schema({
     url: String,
     filename: String
@@ -39,6 +43,11 @@ const CampgroundSchema = new Schema({
             ref: 'Review'
         }
     ]
+},opts);
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+    return `
+    <strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
+    <p>${this.description.substring(0, 20)}...</p>`
 });
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
     if (doc) {
